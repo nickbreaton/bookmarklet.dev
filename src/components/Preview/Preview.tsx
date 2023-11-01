78a9f39c6b6f3022b4cbf1ce854cb9bc980f7b82
Nick Breaton <nick@nickbreaton.com>
2023-11-01 00:06:59 -0400

    execute JS in frame as response to event

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,10 +1,42 @@
-import { createEffect, useContext } from "solid-js"
+import { createEffect, createSignal, useContext } from "solid-js"
 import { useBundle } from "../BundleProvider/BundleProvider"
 
 export const Preview = () => {
   const code = useBundle()
+  const [frame, setFrame] = createSignal<HTMLIFrameElement>()
 
-  createEffect(() => console.log(code()))
+  const nonce = crypto.randomUUID()
 
-  return <iframe srcdoc={`<h1>hello</h1><script>${code()}</script>`} />
+  const content = `
+    <h1>hello</h1>
+  `
+
+  const script = /* html */ `
+    <script>
+      window.addEventListener('message', ({ data }) => {
+        if (data.nonce === '${nonce}') {
+          if (data.type === 'run') {
+            eval(data.payload)
+          }
+        }
+      })
+    </script>
+  `
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          frame()?.contentWindow?.postMessage({
+            type: "run",
+            nonce,
+            payload: code(),
+          })
+        }}
+      >
+        Run
+      </button>
+      <iframe ref={setFrame} srcdoc={`${content}${script}`} />
+    </div>
+  )
 }
